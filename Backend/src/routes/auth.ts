940c87b550742600035871f59dd84ebf0ffd2e8b
juan-camilo-11 <thekmilo84@gmail.com>
2024-07-16 16:28:53 -0500

    Ajuste de pruebas

--- Backend/src/routes/auth.ts
+++ Backend/src/routes/auth.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/auth/google", passport.authenticate('google', { scope: ['profile', 'email'] }));
 router.get('/auth/google/callback',
     passport.authenticate('google', { failureRedirect: `${process.env.APP_URL_FRONTEND}/short-url-blink/error`, session: false }),
-    function (req, res) {
+    async function (req, res) {
         try{
             console.log("-Auth")
             res
